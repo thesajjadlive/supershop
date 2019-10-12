21a2fba1fcc5aec8d0baf91a435c3887652c4c59
Sajjad Hossain <sajjad@piit.local>
2019-10-12 10:37:12 +0600

    project initialize and master templeting done

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        $data['title']='Dashboard';
+        return view('backend.dashboard',$data);
+    }
+}
---
+++ resources/views/layouts/back/master.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+@include('layouts.back._head')
+
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    @include('layouts.back._sidebar')
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+
+        <div id="content">
+
+            <!-- Topbar -->
+        @include('layouts.back._header')
+            <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+        @yield('content')
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        @include('layouts.back._footer')
+        <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+    @include('layouts.back._logout_modal')
+<!-- Bootstrap core JavaScript-->
+
+@include('layouts.back._scripts')
+
+</body>
+
+</html>
